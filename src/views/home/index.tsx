0b58e9bdd5576295933c2abc34fc374092dc1c66
Appslk <162972386+appslk@users.noreply.github.com>
2024-09-14 15:24:44 +0530

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -6,9 +6,10 @@ import Link from 'next/link';
 import { useWallet, useConnection } from '@solana/wallet-adapter-react';
 
 // Components
- 
+
 // Store
 import useUserSOLBalanceStore from '../../stores/useUserSOLBalanceStore';
+import { CandyMint } from 'components/CandyMint';
 
 export const HomeView: FC = ({ }) => {
   const wallet = useWallet();
@@ -28,7 +29,8 @@ export const HomeView: FC = ({ }) => {
     <div id="bgC" className="w-full mx-auto flex flex-col items-center">
       <div className="w-full flex flex-col items-center">
         <div className='mt-6 w-full flex justify-center'>
-          <h1 id="mainT" className="text-center text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 mb-4">            YAKA
+          <h1 id="mainT" className="text-center text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 mb-4">
+            YAKA
           </h1>
         </div>
 
@@ -41,6 +43,8 @@ export const HomeView: FC = ({ }) => {
           </p>
         </div>
       </div>
+
+      <CandyMint />
 
     </div>
   );
